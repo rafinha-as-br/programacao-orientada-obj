6299f5acfa6d3e4812bc90728bf4fa2f13e462e1
Rafael Antunes <rafinha72465@gmail.com>
2025-04-25 21:47:25 -0300

    final da aula

--- notaFiscal/src/main/java/br/edu/ifsc/projetonotafiscal/Main.java
+++ notaFiscal/src/main/java/br/edu/ifsc/projetonotafiscal/Main.java
@@ -1,13 +1,22 @@
 package br.edu.ifsc.projetonotafiscal;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     //classes wrappers
-
+    public  static void print (double d){
+        System.out.println(d);
+    }
 
     public static void main(String[] args) {
-        Double valor = 10d;
-        valor+=20;
-        System.out.println(valor);
+        String nomeSupermercado = "TEM SUPERMERCADO";
+        System.out.println(StringUtils.center(nomeSupermercado, 60));
+        System.out.println(StringUtils.leftPad("123", 10));
+        Double total = 10.23;
+        System.out.println(String.format("%2f", total));
+        String nomeProduto = "Nescafe 500g = Selecao especial";
+        System.out.printf(StringUtils.abbreviate(nomeProduto, 30));
+
     }
 
 
